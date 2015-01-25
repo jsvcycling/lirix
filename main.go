95bcaa6934b6c76a2c3c3220346cfa24f65b3a75
jsvcycling <jsvcycling@live.com>
2015-01-25 17:40:37 -0500

    rewrote most of the logic

--- main.go
+++ main.go
@@ -1,261 +1,341 @@
-// Copyright (C) 2015 Josh Vega. All Rights Reserved.
-
 package main
 
 import (
-	"html/template"
-	"io/ioutil"
 	"encoding/json"
 	"net/http"
+	"io/ioutil"
 	"time"
+	"fmt"
 	"strconv"
+	"html/template"
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
 )
 
-type WeatherData struct {
-	LocationName string
+type RawCurrentData struct {
+	LocationId int `json:"id"`
+	UnixTime int `json:"dt"`
+	LocationName string `json:"name"`
+
+	System struct {
+		LocationCountry string `json:"country"`
+		Sunrise int `json:"sunrise"`
+		Sunset int `json:"sunset"`
+	} `json:"sys"`
+
+	Main struct {
+		Temperature float64 `json:"temp"`
+		Humidity float64 `json:"humidity"`
+		Pressure float64 `json:"pressure"`
+	} `json:"main"`
+
+	Wind struct {
+		Speed float64 `json:"speed"`
+		Direction float64 `json:"deg"`
+		Gusts float64 `json:"gust"`
+	} `json:"wind"`
+
+	Clouds struct {
+		Coverage float64 `json:"all"`
+	} `json:"clouds"`
+
+	Weather []struct {
+		Id int `json:"id"`
+		Type string `json:"main"`
+		Description string `json:"description"`
+		IconId string `json:"icon"`
+	} `json:"weather"`
+
+	Rain struct {
+		Amount float64 `json:"3h"`
+	} `json:"rain"`
+
+	Snow struct {
+		Amount float64 `json:"3h"`
+	} `json:"snow"`
+}
+
+type CurrentData struct {
+	DisplayName string
 	LocationId string
 	TargetTime string
 
-	SunriseTime string
-	SunsetTime string
+	Sunrise string
+	Sunset string
+
+	WeatherDescription string
+	WeatherIcon string
 
 	Temperature string
 	Humidity string
+	Pressure string
 
-	WindSpeed float64
+	WindSpeed string
 	WindDirection string
-	WindGusts float64
+	WindGusts string
 
-	CloudCoverage float64
+	CloudCoverage string
 
-	WeatherDescription string
-
-	RainHeight float64
-	SnowHeight float64
+	RainAmount string
+	SnowAmount string
 }
 
-type DetailedWeather struct {
-	LocationName string
+type Location struct {
+	DisplayName string
+}
+
+type RawDetailedData struct {
+	City struct {
+		Id int `json:"id"`
+		Name string `json:"name"`
+		Country string `json:"country"`
+	} `json:"city"`
+
+	Data []struct {
+		UnixTime int `json:"dt"`
+
+		Main struct {
+			Temp float64 `json:"temp"`
+			Humidity float64 `json:"humidity"`
+			Pressure float64 `json:"pressure"`
+		} `json:"main"`
+
+		Wind struct {
+			Speed float64 `json:"speed"`
+			Direction float64 `json:"deg"`
+			Gusts float64 `json:"gust"`
+		} `json:"wind"`
+
+		Clouds struct {
+			Coverage float64 `json:"all"`
+		} `json:"clouds"`
+
+		Weather []struct {
+			Id int `json:"id"`
+			Type string `json:"main"`
+			Description string `json:"description"`
+			IconId string `json:"icon"`
+		} `json:"weather"`
+
+		Rain struct {
+			Amount float64 `json:"3h"`
+		} `json:"rain"`
+
+		Snow struct {
+			Amount float64 `json:"3h"`
+		} `json:"snow"`
+	} `json:"list"`
+}
+
+type DetailedData struct {
+	DisplayName string
 	LocationId string
-	Count int
-	Data []WeatherData
+
+	Forecast []struct {
+		TargetTime string
+
+		WeatherDescription string
+		WeatherIcon string
+
+		Temperature string
+		Humidity string
+		Pressure string
+
+		WindSpeed string
+		WindDirection string
+		WindGusts string
+
+		CloudCoverage string
+	}
 }
 
-func parseTimestamp(t1 float64) string {
-	var t2 time.Time
-	t2 = time.Unix(int64(t1), 0)
-	return t2.Format("Monday, January 2 2006 @ 03:04:05PM (MST)")
+type RawSearchResultData struct {
+	Data []struct {
+		LocationId int `json:"id"`
+		LocationName string `json:"name"`
+
+		System struct {
+			Country string `json:"country"`
+		} `json:"sys"`
+	} `json:"list"`
 }
 
-func parseTime(t1 float64) string {
-	var t2 time.Time
-	t2 = time.Unix(int64(t1), 0)
-	return t2.Format("03:04:05pm (MST)")
+type ResultData struct {
+	LocationId string
+	LocationName string
 }
 
-func parseDateTime(t1 float64) string {
-	var t2 time.Time
-	t2 = time.Unix(int64(t1), 0)
-	return t2.Format("Mon 3:04pm (MST)")
-}
-
-func parseWindDir(dir float64) string {
-	if dir > 360 { dir -= 360 }
-
-	if dir >= 348.75 && dir < 11.25 { return "N" }
-	if dir >= 11.25 && dir < 33.75 { return "NNE" }
-	if dir >= 33.75 && dir < 56.25 { return "NE" }
-	if dir >= 56.25 && dir < 78.75 { return "ENE" }
-	if dir >= 78.75 && dir < 101.25 { return "E" }
-	if dir >= 101.25 && dir < 123.75 { return "ESE" }
-	if dir >= 123.75 && dir < 146.25 { return "SE" }
-	if dir >= 146.25 && dir < 168.75 { return "SSE" }
-	if dir >= 168.75 && dir < 191.25 { return "S" }
-	if dir >= 191.25 && dir < 213.75 { return "SSW" }
-	if dir >= 213.25 && dir < 236.25 { return "SW" }
-	if dir >= 236.25 && dir < 258.75 { return "WSW" }
-	if dir >= 258.75 && dir < 281.25 { return "W" }
-	if dir >= 281.25 && dir < 303.75 { return "WNW" }
-	if dir >= 303.75 && dir < 326.25 { return "NW" }
-	if dir >= 326.25 && dir < 348.75 { return "NNW" }
+func getCurrentData(id string) CurrentData {
+	resp, _ := http.Get("http://api.openweathermap.org/data/2.5/weather?id=" + id)
 	
-	return "Unavailable"
+	var raw_data RawCurrentData
+	body, _ := ioutil.ReadAll(resp.Body)
+	json.Unmarshal(body, &raw_data)
+
+	var ret CurrentData
+	ret.LocationId = strconv.Itoa(raw_data.LocationId)
+
+	var t time.Time
+	t = time.Unix(int64(raw_data.UnixTime), 0)
+	ret.TargetTime = t.Format("Monday, January 02, 2006 @ 03:04:05PM (MST)")
+
+	t = time.Unix(int64(raw_data.System.Sunrise), 0)
+	ret.Sunrise = t.Format("3:04pm (MST)")
+
+	t = time.Unix(int64(raw_data.System.Sunset), 0)
+	ret.Sunset = t.Format("3:04pm (MST)")
+
+	ret.WeatherDescription = raw_data.Weather[0].Description
+	ret.WeatherIcon = raw_data.Weather[0].IconId
+	ret.Temperature = strconv.FormatFloat(raw_data.Main.Temperature - 273.15, 'f', 1, 32)
+	ret.Humidity = strconv.FormatFloat(raw_data.Main.Humidity, 'f', 0, 32)
+	ret.Pressure = strconv.FormatFloat(raw_data.Main.Pressure, 'f', 2, 32)
+	ret.WindSpeed = strconv.FormatFloat(raw_data.Wind.Speed, 'f', 1, 32)
+	ret.WindDirection = strconv.FormatFloat(raw_data.Wind.Direction, 'f', 0, 32)
+	ret.WindGusts = strconv.FormatFloat(raw_data.Wind.Gusts, 'f', 1, 32)
+	ret.CloudCoverage = strconv.FormatFloat(raw_data.Clouds.Coverage, 'f', 0, 32)
+	ret.RainAmount = strconv.FormatFloat(raw_data.Rain.Amount, 'f', 0, 32)
+	ret.SnowAmount = strconv.FormatFloat(raw_data.Snow.Amount, 'f', 0, 32)
+
+	return ret
 }
 
-func parseWeatherData(data interface{}, timestamp bool) WeatherData {
-	var ret WeatherData
+func getDetailedData(id string) DetailedData {
+	resp, _ := http.Get("http://api.openweathermap.org/data/2.5/forecast?id=" + id)
 
-	tmp := data.(map[string]interface{})
-	for key, val := range tmp {
-		switch key {
-		case "dt":
-			if (timestamp) {
-				ret.TargetTime = parseTimestamp(val.(float64))
-			} else {
-				ret.TargetTime = parseDateTime(val.(float64))
-			}
-		case "sys":
-			val2 := val.(map[string]interface{})
-			for key3, val3 := range val2 {
-				switch key3 {
-				case "sunrise":
-					ret.SunriseTime = parseTime(val3.(float64))
-				case "sunset":
-					ret.SunsetTime = parseTime(val3.(float64))
-				}
-			}
-		case "main":
-			val2 := val.(map[string]interface{})
-			for key3, val3 := range val2 {
-				switch key3 {
-				case "temp":
-					ret.Temperature = strconv.FormatFloat(val3.(float64) - 273.15, 'f', 2, 32)
-				case "humidity":
-					ret.Humidity = strconv.FormatFloat(val3.(float64), 'f', 0, 32)
-				}
-			}
-		case "weather":
-			val2 := val.([]interface{})
-			val3 := val2[0].(map[string]interface{})
-			for key4, val4 := range val3 {
-				switch key4 {
-				case "description":
-					ret.WeatherDescription = val4.(string)
-				}
-			}
-		case "wind":
-			val2 := val.(map[string]interface{})
-			for key3, val3 := range val2 {
-				switch key3 {
-				case "speed":
-					ret.WindSpeed = val3.(float64)
-				case "deg":
-					ret.WindDirection = parseWindDir(val3.(float64))
-				case "gust":
-					ret.WindGusts = val3.(float64)
-				}
-			}
-		case "clouds":
-			val2 := val.(map[string]interface{})
-			for key3, val3 := range val2 {
-				switch key3 {
-				case "all":
-					ret.CloudCoverage = val3.(float64)
-				}
-			}
-		case "rain":
-			val2 := val.(map[string]interface{})
-			for key3, val3 := range val2 {
-				switch key3 {
-				case "3h":
-					ret.RainHeight = val3.(float64)
-				}
-			}
-		case "snow":
-			val2 := val.(map[string]interface{})
-			for key3, val3 := range val2 {
-				switch key3 {
-				case "3h":
-					ret.SnowHeight = val3.(float64)
-				}
-			}
-		}
+	var raw_data RawDetailedData
+	body, _ := ioutil.ReadAll(resp.Body)
+	json.Unmarshal(body, &raw_data)
+
+	var ret DetailedData
+	ret.LocationId = strconv.Itoa(raw_data.City.Id)
+
+	ret.Forecast = make([]struct {
+		TargetTime string
+		WeatherDescription string
+		WeatherIcon string
+		Temperature string
+		Humidity string
+		Pressure string
+		WindSpeed string
+		WindDirection string
+		WindGusts string
+		CloudCoverage string
+	}, len(raw_data.Data))
+
+	for i := 0; i < len(raw_data.Data); i++ {
+		var t time.Time
+		t = time.Unix(int64(raw_data.Data[i].UnixTime), 0)
+		ret.Forecast[i].TargetTime = t.Format("Mon 3:04pm (MST)")
+
+		ret.Forecast[i].WeatherDescription = raw_data.Data[i].Weather[0].Description
+		ret.Forecast[i].WeatherIcon = raw_data.Data[i].Weather[0].IconId
+		ret.Forecast[i].Temperature = strconv.FormatFloat(raw_data.Data[i].Main.Temp - 273.15, 'f', 1, 32)
+		ret.Forecast[i].Humidity = strconv.FormatFloat(raw_data.Data[i].Main.Humidity, 'f', 0, 32)
+		ret.Forecast[i].Pressure = strconv.FormatFloat(raw_data.Data[i].Main.Pressure, 'f', 2, 32)
+		ret.Forecast[i].WindSpeed = strconv.FormatFloat(raw_data.Data[i].Wind.Speed, 'f', 1, 32)
+		ret.Forecast[i].WindDirection = strconv.FormatFloat(raw_data.Data[i].Wind.Direction, 'f', 0, 32)
+		ret.Forecast[i].WindGusts = strconv.FormatFloat(raw_data.Data[i].Wind.Gusts, 'f', 1, 32)
+		ret.Forecast[i].CloudCoverage = strconv.FormatFloat(raw_data.Data[i].Clouds.Coverage, 'f', 0, 32)
 	}
 
 	return ret
 }
 
-func parseCurrent(res *http.Response) WeatherData {
-	body, _ := ioutil.ReadAll(res.Body)
+func getSearchResultData(query string) []ResultData {
+	resp, _ := http.Get("http://api.openweathermap.org/data/2.5/find?q=" + query + "&type=accurate")
 
-	var tmp interface{}
-	json.Unmarshal(body, &tmp)
+	var raw_data RawSearchResultData
+	body, _ := ioutil.ReadAll(resp.Body)
+	json.Unmarshal(body, &raw_data)
 
-	return parseWeatherData(tmp, true)
-}
+	ret := make([]ResultData, len(raw_data.Data))
 
-func parseDetailed(res *http.Response) DetailedWeather {
-	var ret DetailedWeather
-
-	body, _ := ioutil.ReadAll(res.Body)
-	var tmp interface{}
-	json.Unmarshal(body, &tmp)
-
-	data := tmp.(map[string]interface{})
-	for key, val := range data {
-		switch key {
-		case "cnt":
-			ret.Count = int(val.(float64))
-		case "list":
-			val2 := val.([]interface{})
-			ret.Data = make([]WeatherData, ret.Count)
-			for i := 0; i < ret.Count; i++ {
-				ret.Data[i] = parseWeatherData(val2[i], false)
-			}
-		}
+	for i := 0; i < len(raw_data.Data); i++ {
+		ret[i].LocationId = strconv.Itoa(raw_data.Data[i].LocationId)
+		ret[i].LocationName = raw_data.Data[i].LocationName + ", " + raw_data.Data[i].System.Country
 	}
 
 	return ret
 }
 
 func main() {
-	locations := make(map[string]string)
+	fmt.Println("Go to http://localhost:3000 to view Lirix.")
 
-	var templates = template.Must(template.ParseGlob("templates/*"))
+	db, _ := sql.Open("sqlite3", "./data.db")
 
-	// Sample data
-	locations["5128581"] = "New York City, New York"
-	locations["4140963"] = "Washington, D.C."
-	locations["5368361"] = "Los Angeles, California"
-	locations["4259418"] = "Indianapolis, Indiana"
-	locations["4684888"] = "Dallas, Texas"
-	locations["6077243"] = "Montreal, Québec"
-	locations["3169070"] = "Rome, Italy"
-	locations["2643743"] = "London, England"
-	locations["2759794"] = "Amsterdam, Netherlands"
+	var templates = template.Must(template.ParseGlob("views/*"))
 
-	// Shows the current weather information for each of the selected locations.
 	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
-		weather := make([]WeatherData, len(locations))
+		rows, _ := db.Query("SELECT * FROM locations;")
+		cnt, _ := db.Query("SELECT COUNT(*) FROM locations;")
+		cnt.Next()
+		var count int
+		cnt.Scan(&count)
+
+		weather := make([]CurrentData, count)
 
 		i := 0
-		for id, name := range locations {
-			resp, _ := http.Get("http://api.openweathermap.org/data/2.5/weather?id=" + id)
-			weather[i] = parseCurrent(resp)
-			weather[i].LocationName = name
-			weather[i].LocationId = id
+		for rows.Next() {
+			var id string
+			var display_name string
+			rows.Scan(&id, &display_name)
+
+			weather[i] = getCurrentData(id)
+			weather[i].DisplayName = display_name
 			i++
 		}
 
-		data := map[string]interface{} {
+		tmpl_data := map[string]interface{} {
 			"title": "Lirix | Overview",
-			"weatherdata": weather,
+			"data": weather,
 		}
 
-		templates.ExecuteTemplate(res, "index", data)
+		templates.ExecuteTemplate(res, "index", tmpl_data)
 	})
 
-	http.HandleFunc("/about", func(res http.ResponseWriter, req *http.Request) {
-		templates.ExecuteTemplate(res, "about", nil)
+	http.HandleFunc("/search_results", func(res http.ResponseWriter, req *http.Request) {
+		results := getSearchResultData(req.URL.Query().Get("q"))
+
+		tmpl_data := map[string]interface{} {
+			"title": "Lirix | Search Results",
+			"query": req.URL.Query().Get("q"),
+			"data": results,
+		}
+
+		templates.ExecuteTemplate(res, "search_results", tmpl_data)
 	})
 
-	// Get detailed information about a single location.
 	http.HandleFunc("/detail", func(res http.ResponseWriter, req *http.Request) {
-		qs := req.URL.Query()
+		result := getDetailedData(req.URL.Query().Get("id"))
 
-		var weather DetailedWeather
+		row, _ := db.Query("SELECT display_name FROM locations WHERE id = \"" + req.URL.Query().Get("id") + "\" LIMIT 1")
+		row.Next()
+		var display_name string
+		row.Scan(&display_name)
 
-		resp, _ := http.Get("http://api.openweathermap.org/data/2.5/forecast?id=" + qs.Get("location"))
-		weather = parseDetailed(resp)
-		weather.LocationName = locations[qs.Get("location")]
-		weather.LocationId = qs.Get("location")
+		result.DisplayName = display_name
 
-		data := map[string]interface{} {
-			"title": "Lirix | Detail",
-			"weather": weather,
+		tmpl_data := map[string]interface{} {
+			"data": result,
 		}
 
-		templates.ExecuteTemplate(res, "detail", data)
+		templates.ExecuteTemplate(res, "detail", tmpl_data)
+	})
+
+	http.HandleFunc("/add", func(res http.ResponseWriter, req *http.Request) {
+		db.Exec("INSERT INTO locations (id, display_name) VALUES('" + req.URL.Query().Get("id") + "', '" + req.URL.Query().Get("name") + "');", nil)
+
+		fmt.Println(req.URL.Query().Get("id"))
+		fmt.Println(req.URL.Query().Get("name"))
+
+		http.Redirect(res, req, "/", 301)
+	})
+
+	http.HandleFunc("/delete", func(res http.ResponseWriter, req *http.Request) {
+		db.Exec("DELETE FROM locations WHERE id = \"" + req.URL.Query().Get("id") + "\";")
+
+		http.Redirect(res, req, "/", 301)
 	})
 
 	http.ListenAndServe(":3000", nil)
